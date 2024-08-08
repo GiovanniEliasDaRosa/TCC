8d5019f2a91a885def938443ed065e7c1ec3a70f
tiago citrangulo da silva <otavio.azevedo.botelho@gmail.com>
2024-08-08 08:27:31 -0300

    primeiro update
    descrição mais detalhada na proxima alteração.

--- admin/avisos/edicao.php
+++ admin/avisos/edicao.php
@@ -1,14 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Avisos - Edição</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edição de Aviso</title>
 </head>
-
+<?php
+    if(isset($_GET['id']))
+    {
+        $id=$_GET['id'];
+        include_once('../Conexao.php');
+        $sql="SELECT * FROM tb_aviso WHERE id=$id";
+        $query=mysqli_query($con,$sql);
+        $ret=mysqli_fetch_assoc($query);
+        $id=$ret['id'];
+        $titulo=$ret['titulo'];
+        $corpo=$ret['corpo'];
+        $dia=$ret['dia'];
+        $exp=$ret['exp'];
+    }
+    ?>
 <body>
-  <h1>Avisos - Edição</h1>
-</body>
+    <h1>Edição de Aviso</h1>
+    <form method='post' action="AltAviso.php" enctype='multipart/form-data'>
+        <p><input type="hidden" name="id" value="<?php echo $id;?>"></p>
 
+        <p><label for="titulo">TíTulo</label>
+        <input type="text" name="titulo" value="<?php echo $titulo;?>"></p>
+
+        <p><label for="corpo">Corpo</label>
+        <textarea name="corpo" value="<?php echo $corpo;?>"></textarea></p>
+
+        <p><label for="dia">Data da Postagem</label>
+        <input type="date" name="dia" value="<?php echo $dia;?>"></p>
+
+        <p><label for="exp">Data de Expiração</label>
+        <input type="date" name="exp" value="<?php echo $exp;?>"></p>
+
+        <p><input type="submit" name="salvar" value="salvar">
+        <input type="button" value="Voltar" onclick="history.back()">
+        <button><a href='ExAviso.php?id=<?php echo $id;?>'>Excluir</a></button></p>
+    </form>
+    </div>
+</body>
 </html>
--- admin/avisos/vizualizacao.php
+++ admin/avisos/vizualizacao.php
@@ -1,16 +1,44 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Avisos - Vizualizção</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Avisos</title>
+    <style>
+        tr,td{
+            border: 1px solid;
+            margin: 4px;
+        }
+    </style>
 </head>
-
 <body>
-  <h1>Avisos - Vizualizção</h1>
-  <a href="adicao.php">Adição</a>
-  <a href="edicao.php">Edição</a>
+    <?php
+    date_default_timezone_set('America/Sao_Paulo');
+    echo '<meta charset="UTF-8">';
+    include ('..\Conexao.php');
+    $sql="SELECT titulo,id,corpo,inicio,fim FROM tb_aviso ORDER BY inicio DESC";
+    $query=mysqli_query($con,$sql);
+    while($ret=mysqli_fetch_array($query)){
+        $date=date('Y-m-d');
+        $titulo=$ret['titulo'];
+        $corpo=$ret['corpo'];
+        $id=$ret['id'];
+        $inicio=$ret['inicio'];
+        $fim=$ret['fim'];
+        if($date>=$dia && $date<=$exp){
+            echo "<table> <tr> <td>$titulo $inicio</td><td><a href='EditAviso.php?id=$id'>Editar</a> ";
+            if($corpo!=null){
+                echo "<td><a href='MaisAviso.php?id=$id'>Ler Mais</a></td></tr></table>";
+            }else{
+                echo "</tr></table>";}
+        }elseif($date>=$exp){
+            include_once('../Conexao.php');
+            $delsql="DELETE FROM tb_aviso WHERE id=$id";
+            $delquery=mysqli_query($con,$delsql) or die (mysqli_error($con));
+        }else{}
+        }
+        echo "<table><tr><td><a href='AddAviso.php'>Adicionar aviso</a></td></tr></table>";
+    mysqli_close($con)
+    ?>
 </body>
-
 </html>
