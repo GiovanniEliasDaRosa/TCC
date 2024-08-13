3ea6b887de7318204730c0d4afc0769f31352669
Tiago Citrangulo da Silva <101050472+GiovanniEliasDaRosa@users.noreply.github.com>
2024-08-13 11:29:53 -0300

    Update Conexao.php

--- Conexao.php
+++ Conexao.php
@@ -1,12 +1,20 @@
 <?php
-$svr='127.0.0.1';
-$usuario='root';
-$senha='';
-$banco='HorarioEscolarDB';
-$con=@mysqli_connect($svr,$usuario,$senha,$banco)
-or die('Contate o administrador - não foi possivel conectar a base de dados!');
-mysqli_query($con,"SET NAMES 'utf8'");
-mysqli_query($con,'SET character_set_connection=utf8');
-mysqli_query($con,'SET character_set_client=utf8');
-mysqli_query($con,'SET character_set_results=utf8');
-?>
+$svr = '127.0.0.1';
+$usuario = 'root';
+$senha = '';
+$banco = 'HorarioEscolarDB';
+
+try {
+  $con = @mysqli_connect($svr, $usuario, $senha, $banco);
+} catch (Exception $e) {
+  if (mysqli_connect_error()) {
+    $response = mysqli_connect_error();
+  }
+  echo "<h1>Contate o administrador - não foi possivel conectar a base de dados!</h1><br>";
+  exit();
+}
+
+mysqli_query($con, "SET NAMES 'utf8'");
+mysqli_query($con, "SET character_set_connection=utf8");
+mysqli_query($con, "SET character_set_client=utf8");
+mysqli_query($con, "SET character_set_results=utf8");
