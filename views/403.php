a1772045acd7758a831da2c962a6254ec131ed98
GiovanniBolivoro <giovanniedarosa@gmail.com>
2024-09-21 17:05:09 -0300

    feat: Melhorei a tela de erro 404
    - Atualizei o design da página de erro 404.
    - Adicionei uma imagem de um personagem pixel art confuso.

--- views/403.php
+++
@@ -1,9 +0,0 @@
-<?php require('partials/head.php') ?>
-<?php require('partials/usernav.php') ?>
-
-<main>
-  <h1>Você não é autorizado para ver essa página.</h1>
-  <a href="/">Ir para o menu principal.</a>
-</main>
-
-<?php require('partials/footer.php') ?>
--- views/404.php
+++ views/404.php
@@ -1,9 +1,21 @@
-<?php require('partials/head.php') ?>
-<?php require('partials/usernav.php') ?>
+<!DOCTYPE html>
+<html lang="pt-br">
 
-<main>
-  <h1>Página não encontrada</h1>
-  <a href="/">Ir para o menu principal.</a>
-</main>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Editar Avisos Admin</title>
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
+  <link rel="stylesheet" type="text/css" href="/css/404error.css">
+</head>
 
-<?php require('partials/footer.php') ?>
+<body>
+  <main>
+    <img src="/img/404error.png" alt="Alguém perdido em pixel art">
+    <h1 id="error">404</h1>
+    <h1>Página não encontrada</h1>
+    <a href="/">Ir para o menu principal.</a>
+  </main>
+</body>
+
+</html>
